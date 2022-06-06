7e1166a5e202e1cfe25596999ed37c3b2632ee41
shyam-lal <slal4252@gmail.com>
2022-06-06 15:59:59 +0530

    Initial commit

---
+++ SonarQubeTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SonarQubeTest
+//
+//  Created by Shyamlal on 02/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("sadasdas")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
